00a3cb80668cfdece30ab0a0e95f88b2e9dd82e9
Pauline Moraa <pmoraa431@gmail.com>
2025-03-16 11:57:52 +0300

    Build a functioning to-do list application Suppress a default action with event.preventDefault()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  //getting form element
+  let form = document.querySelector('form');
+  //adding eventlistener submit
+  form.addEventListener('submit', (e)=>{
+    //prevents form submission
+    e.preventDefault(); 
+     //getting task description form
+    let taskDescription=e.target.querySelector('#new-task-description').value
+    //calling function
+    buildNewTask(taskDescription)
+    //reseting the form 
+    form.reset();
+
+  });  
+  });
+
+  function  buildNewTask(taskDescription){
+    //lets create a new list item
+    let taskItem= document.createElement('li')
+    let taskList = document.getElementById('tasks')
+//creating a delete button 'x'
+let deleteButton = document.createElement('button')
+deleteButton.textContent= 'x';
+deleteButton.addEventListener('click', handleDelete)
+taskItem.textContent = taskDescription;
+taskItem.appendChild(deleteButton);
+taskList.appendChild(taskItem)
+
+//styling the x button 
+deleteButton.style.color = 'red';
+deleteButton.style.fontSize = '12px';
+deleteButton.style.cursor = 'pointer';
+
+  }
+  
+function handleDelete(e){
+  //removing parent list item
+e.target.parentNode.remove();
+}
